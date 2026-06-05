27b17b819baf5bc24de2cc3dfd054bbcf545b1ec
parameswar_Elamaran <parameswarbe2099@gmail.com>
2026-06-05 07:30:07 +0530

    Added like feature

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 
 function Hero(props) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+  return Number(localStorage.getItem("portfolioLikes")) || 0;
+});
   const [message, setMessage] = useState("");
 
 useEffect(() => {
@@ -30,21 +32,24 @@ useEffect(() => {
       <h3>{message}</h3>
 
       <h3>Likes: {count}</h3>
-
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-          borderRadius: "8px",
-          border: "none",
-          backgroundColor: "#222",
-          color: "white",
-        }}
-      >
-        Like 👍
-      </button>
+<button
+  onClick={() => {
+    const newCount = count + 1;
+    setCount(newCount);
+    localStorage.setItem("portfolioLikes", newCount);
+  }}
+  style={{
+    padding: "10px 20px",
+    fontSize: "16px",
+    cursor: "pointer",
+    borderRadius: "8px",
+    border: "none",
+    backgroundColor: "#222",
+    color: "white",
+  }}
+>
+  Like 👍
+</button>
     </section>
   );
 }
